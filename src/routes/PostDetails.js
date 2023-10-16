69243a08e1762dcc14607a95659cffbb246c55df
Shakstetic <amshyaswad@gmail.com>
2023-10-17 02:26:06 +0800

    added discussionmembers column

--- src/routes/PostDetails.js
+++ src/routes/PostDetails.js
@@ -1,16 +1,25 @@
 import React from "react";
 import { useLocation } from 'react-router-dom';
+import Divider from '@mui/material/Divider';
 
 export default function PostDetails() {
   let { title, description, postOwner } = useLocation().state;
 
   return (
-    <div className="content">
-      <h1>{title}</h1>
-      <h2>Overview</h2>
-      <p>{description}</p>
-      <p>Posted by: {postOwner.name}</p>
-      <p>Role: {postOwner.role}</p>
+    <div style={{display: "flex"}}>
+      <div className="content" style={{flex: 4}}>
+        <h1>{title}</h1>
+        <h2>Overview</h2>
+        <p>{description}</p>
+        <Divider/>
+        <h2>Discussion</h2> 
+      </div>
+      <div className="discussionMembers" style={{flex: 1}}>       
+        <h2>Posted By</h2>
+        <p>{postOwner.name}</p>
+        <p>{postOwner.role}</p>
+        <h2>Participants</h2>
+      </div>
     </div>
   );
 }
