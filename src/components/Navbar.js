f2667d2fe95712b8bfc9a8e7fc476c815b2abcc2
Shakstetic <amshyaswad@gmail.com>
2023-11-26 15:18:35 +0800

    edited logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,14 @@ import { Link } from "react-router-dom";
 import { SidebarData } from "./SidebarData";
 import "../App.css";
 import { IconContext } from "react-icons";
+import logo from "../components/logo.png"
 
 function Navbar() {
   return (
     <>
       <IconContext.Provider value={{ color: "undefined" }}>
         <div className="navbar" style={{ position: "fixed", top: 0, left: 0, right: 0, zIndex: 1 }}>
-          <h1>DisCoLabs</h1>
+          <img src={logo} alt="DisCoLabs" style={{height: 50}}/>
         </div>
         <nav className="nav-menu active">
           <ul className="nav-menu-items">
